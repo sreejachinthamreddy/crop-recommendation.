170bcfb8b4c6fa645e691afa32860868e1d4f40c
19121A0544 <78897938+sreejachinthamreddy@users.noreply.github.com>
2022-03-24 18:02:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import numpy as np
+from flask import *
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('traintest.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('one.html')
+@app.route('/croprecommend',methods=['GET','POST'])
+def recommend():
+    return render_template('two.html')
+@app.route('/output',methods=['GET','POST'])
+def output():
+    float_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(float_features)]
+    pred = model.predict(final_features) 
+    return render_template('result.html', prediction = pred)
+
+if __name__ == "__main__":
+    app.run(debug=True)
